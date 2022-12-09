efb058199408f0040ecffeef2ad5515c46ca8404
unknown <osama_magdy0@yahoo.com>
2022-12-09 20:25:38 +0200

    the second commit

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const addTaskEl = document.querySelector(".add-Task");
 const medialTaskEl = document.querySelector(".medial-con");
 const inputEl = document.querySelector(".input");
-// console.log(addTaskEl);
+const btnEl = document.querySelector(".btn");
+// console.log(btnEl);
 
 addTaskEl.addEventListener("click", () => {
   taskCreation();
 });
 
 function taskCreation() {
+  // creat Task
   const newTaskEl = document.createElement("div");
   newTaskEl.classList.add("createdTask");
   medialTaskEl.appendChild(newTaskEl);
@@ -20,4 +22,36 @@ function taskCreation() {
   newDeleteEl.classList.add("delete");
   newDeleteEl.innerHTML = `Delete`;
   newTaskEl.append(newDeleteEl);
+  inputEl.value = ``;
+
+  // Edit Task
+  newEditEl.addEventListener("click", () => {
+    newEditEl.innerHTML = `Save`;
+    const newinputEl = document.createElement("input");
+    newinputEl.value = newTaskEl.innerText.slice(
+      0,
+      newinputEl.value.length - 11
+    );
+
+    newinputEl.classList.add("input-task");
+    newTaskEl.appendChild(newinputEl);
+    newinputEl.focus();
+
+    newEditEl.addEventListener("click", () => {
+      newEditEl.innerHTML = `Edit`;
+      // newTaskEl.innerHTML = newinputEl.value;
+      // newinputEl.remove();
+      // newinputEl.style.display = "none";
+    });
+  });
+
+  // Delete Task
+  newDeleteEl.addEventListener("click", () => {
+    newTaskEl.remove();
+  });
+
+  // Delete All Task
+  btnEl.addEventListener("click", () => {
+    newTaskEl.remove();
+  });
 }
